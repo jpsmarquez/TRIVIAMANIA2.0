129f15179aa92f3ebd67f162037bb0b5665a5f47
jpsmarquez <jp.sanchez.marquez@gmail.com>
2019-06-21 14:49:52 -0500

    FINAL  commit

--- triviamania.js
+++ triviamania.js
@@ -5,34 +5,14 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-
-var elem = document.getElementById('canvas'),
-   elemLeft = elem.offsetLeft,
-   elemTop = elem.offsetTop,
-   context = elem.getContext('2d'),
-   elements = [];
-
-// Add event listener for `click` events.
-elem.addEventListener('click', function(event) {
-   var x = event.pageX - elemLeft,
-       y = event.pageY - elemTop;
-   console.log(x, y);
-   elements.forEach(function(element) {
-       if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
-           alert('EUREKA');
-       }
-   });
-
-}, false);
-
-
-
 const button1 = document.querySelector("#button1")
-const button2 = document.querySelector("#button2")
-
-const hiddenbuttons = document.querySelector("#hiddenbuttons")
-
 const startOverButton = document.querySelector("#startOverButton")
+const nextQuestionsButton = document.querySelector("#nextButton")
+const nextQuestionsButton1 = document.querySelector("#nextButton1")
+const nextQuestionsButton2 = document.querySelector("#nextButton2")
+const nextQuestionsButton3 = document.querySelector("#nextButton3")
+
+
 
 startOverButton.onclick = function() {
   location.reload();
@@ -40,31 +20,52 @@ startOverButton.onclick = function() {
 
 button1.onclick = function() {
   canvas.style.display = "flex"
+  showTextfirstq(Trivia)
   button1.style.display = "none"
-  button2.style.display = "none"
   startOverButton.style.display = "flex"
+  nextQuestionsButton.style.display = "flex"
 };
 
-button2.onclick = function() {
-  hiddenbuttons.style.display = "flex"
-  button1.style.display = "none"
-  button2.style.display = "none"
+nextQuestionsButton.onclick = function() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  showTextsecondtq(Trivia)
+  nextQuestionsButton.style.display = "none"
+  nextQuestionsButton1.style.display = "flex"
+  questionIndex++
+
 };
 
-buttoneasy.onclick = function() {
-  canvas.style.display = "flex"
-  hiddenbuttons.style.display = "none"
-}
+nextQuestionsButton1.onclick = function() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  
+  showText3rdq(Trivia)
+  nextQuestionsButton1.style.display = "none"
+  nextQuestionsButton2.style.display = "flex"
+  questionIndex++
 
-buttonmedium.onclick = function() {
-  canvas.style.display = "flex"
-  hiddenbuttons.style.display = "none"
-}
+};
 
-buttonhard.onclick = function() {
-  canvas.style.display = "flex"
-  hiddenbuttons.style.display = "none"
-}
+nextQuestionsButton2.onclick = function() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  nextQuestionsButton2.style.display = "none"
+  nextQuestionsButton3.style.display = "flex"
+  showText4thq(Trivia)
+  questionIndex++
+
+};
+
+
+nextQuestionsButton3.onclick = function() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  nextQuestionsButton3.style.display = "none"
+  ctx.font = '60px Arial'
+  ctx.fillText(`SACASTE ${scorePlayer1}/4`,60,100)
+  questionIndex++
+
+};
+
+
+
 
 //_________________________________________________________
 //VARIABLES
@@ -75,80 +76,76 @@ let player2 = []
 
 let scorePlayer1 = 0;
 let scorePlayer2 = 0;
-
-let questionss= [
-    "¿Quienes son los integrantes de los Beatles?",
-    "¿Cuantas notas hay en una octava? ",
-    "¿Quien compuso 'Für Elise'?",
-    "¿Cual fue la primera pelicula a color?",
-    "¿Quien dirigio Titanic(1998)?",
-    "¿Como se llama la protagonista de la pelicula 'Alien'?",
-    "¿Cual fue el primer videojuego?",
-    "¿Que vino primero el Ajedrez o Las Damas Chinas? ",
-    "¿Quien invento Nintendo?"]
-
-let Answers = [[["☐ John Lennon,Ringo Starr, Paul McCartney, George Harrison"],["☐ Kurt Cobain,David Grohl, Krist Novoselic"],["☐ Mick Jagger, Keith Richards,Ronnie Wood,Charlie Watts "]]
-,[["☐ 8"],["☐ 13"],["☐ 12"]],
-[["☐ Beethoven"],["☐ Bach"],["☐ Chopin"]],
-[["☐ Blanca Nieves"],["☐ Ciudadano Kane"],["☐ El Mago de Oz"]],
-[["☐ Stanley Kubrick"],["☐ James Cameron"],["☐ Steven Spielberg"]],
-[["☐ Sigourney Weaver"],["☐ Julia Roberts"],["☐ Winona Rider"]],
-[["☐ PAC-MAN"],["☐ PONG"],["☐ SPACE INVADERS"]],
-[["☐ El Ajedrez"],["☐ Las Damas"],["☐ Los dos se inventaron al mismo tiempo"]],
-[["☐ Satoru Iwata"],["☐ Fusajiro Yamauchi"],["☐ Shigeru Miyamoto"]]]
-
-let suddenDeathQuestions = [
-  "¿Cual fue la primera pelicula con sonido?",
-  "¿Cual nota sigue despues de la nota Re#? ",
-  "¿Cuando se creo FortNite?"]
-let suddenDeathAnswers = [(["Avatar"],["Un Viaje a la Luna"],["The Jazz Singer"]),
-(["Mi"],["Fa"],["Sol#"]),
-(["2018"],["2001"],["2017"])]
-
-
-let rightAnswers = [((Answers[0][0]),(Answers[1][1]),(Answers[2][1])),
-    ((Answers[3][2]),(Answers[4][1]),(Answers[5][0])),
-    ((Answers[6][1]),(Answers[7][1]),(Answers[8][1]))]
-
-let rightAnswersSuddenDeath = [suddenDeathAnswers[0][2],suddenDeathAnswers[1][0],suddenDeathAnswers[2][2]]
-
-
-questionss.forEach ((e,i)=> {
-  ctx.font = "20px Georgia";
-
-  ctx.fillText(e, 10, 50+(100*i));
-  
-})
-
-Answers.forEach ((e,i,a)=> {
-  ctx.font = "15px Georgia";
-  
-  ctx.fillText(e[0], 10, 80+(100*i));
-  ctx.fillText(e[1], 10, 100+(100*i));
-  ctx.fillText(e[2], 10, 120+(100*i));
+let questionIndex = 0;
+let inputPrimero = "";
+let Trivia = {
+  question:[['¿Quienes son los integrantes de los Beatles?',0],['¿Cuantas notas hay en una octava?',1],["¿Cual fue la primera pelicula a color?",2],["¿Cual fue el primer videojuego?",1],["¿Que vino primero el Ajedrez o Las Damas Chinas? ",4],["¿Quien dirigio Titanic(1998)?",5]],
+  answers:[['1) John Lennon,Ringo Starr, Paul McCartney, George Harrison'],['2) Kurt Cobain,David Grohl,Krist Novoselic'],['3) Mick Jagger, Keith Richards,Ronnie Wood,Charlie Watts '],
+  [['1) 8'],['2) 13'],['3) 12']],
+  [["1) Blanca Nieves"],["2) Ciudadano Kane"],["3) El Mago de Oz"]],
+  [["1) PAC-MAN"],["2) PONG"],["3) SPACE INVADERS"]], 
+  [["1) El Ajedrez"],["2) Las Damas"],["3) Los dos se inventaron al mismo tiempo"]],
+  [["1) Stanley Kubrick"],["2) James Cameron"],["3) Steven Spielberg"]]]
  
-})
+};
 
 
-let suddenDeathPlayer1Time = 0
-let suddenDeathPlayer2Time = 0
+
+//console.log(Trivia.question[0].toString())
 
 
-let scorePlayer1SD = 0;
-let scorePlayer2SD = 0;
+function showTextfirstq(Trivia)  {
+ ctx.font = "20px Georgia";
+ ctx.fillText(Trivia.question[0][0], 100, 60) 
+ ctx.fillText(Trivia.answers[0], 30, 100) 
+ ctx.fillText(Trivia.answers[1], 30, 120) 
+ ctx.fillText(Trivia.answers[2], 30, 140) 
 
-var players = []
+}
+
+function showTextsecondtq(Trivia)  {
+  ctx.font = "20px Georgia";
+  ctx.fillText(Trivia.question[1][0], 100, 60) 
+  ctx.fillText(Trivia.answers[3][0], 30, 100) 
+  ctx.fillText(Trivia.answers[3][1], 30, 120) 
+  ctx.fillText(Trivia.answers[3][2], 30, 140) 
+ }
+
+ function showText3rdq(Trivia)  {
+  ctx.font = "20px Georgia";
+  ctx.fillText(Trivia.question[2][0], 100, 60) 
+  ctx.fillText(Trivia.answers[4][0], 30, 100) 
+  ctx.fillText(Trivia.answers[4][1], 30, 120) 
+  ctx.fillText(Trivia.answers[4][2], 30, 140) 
+ 
+ }
+ 
+ function showText4thq(Trivia)  {
+   ctx.font = "20px Georgia";
+   ctx.fillText(Trivia.question[3][0], 100, 60) 
+   ctx.fillText(Trivia.answers[5][0], 30, 100) 
+   ctx.fillText(Trivia.answers[5][1], 30, 120) 
+   ctx.fillText(Trivia.answers[5][2], 30, 140) 
+  }
+  function showText5thq(Trivia)  {
+    ctx.font = "20px Georgia";
+    ctx.fillText(Trivia.question[4][0], 100, 60) 
+    ctx.fillText(Trivia.answers[6][0], 30, 100) 
+    ctx.fillText(Trivia.answers[6][1], 30, 120) 
+    ctx.fillText(Trivia.answers[6][2], 30, 140) 
+   
+   }
+
+
+
 let winner = []
 
 //_________________________________________________________
 //FUNCTIONS
 //_________________________________________________________
 
-
-function startGame(players) {
-    PvP = players[1] === false
-    PvC = players[1] === true
-    
+/*
+function startGame(player1, player2) {
     //starts the game and select if pvp or pvc
 }
 
@@ -162,18 +159,48 @@ function questions(answersP1,answersP2) {
     //adds the score
     //if P2 is CPU, answers on random(difficulty levels(easy,medium,hard) depend on how much correct answers are previously given before the random code)
     //does the algorithm without showing the questions
+}
+
+function Winner(player) {
+
+}
+*/
+
+
+function checkAnswer(){
+ if(inputPrimero === Trivia.question[questionIndex][1]){  
+  scorePlayer1 += 1
+  draw()
+ }
+}
+
+function draw() {
+  ctx.fillStyle = "black"
+  ctx.font = '20px Arial'
+
+  ctx.fillText(`SCORE :${scorePlayer1}`,20,30)
+  ctx.clearRect(40,60)
+  
+}
+
+addEventListener('keydown', function(event){
+  if (event.keyCode === 49 || event.keyCode === 50 || event.keyCode === 51) {
     
-    function suddenDeath(tie=true,answersP1,answersP2) {
-        //checks if the game is in a tie
-        //load the questions and recieves the answers
-        //starts adding to score and to time
-        //compares the scores and times between the player and prints out the winner if we outscore the other one
-    }
-}
-function startOver(gameOver) {
-    //if theres a winner starts over the game, relod the page
-}
+      inputPrimero = event.keyCode - 49
+      checkAnswer()
+  
 
+  }
+})
 
-
-
+//addEventListener('keydown', function(event){
+//  if (event.keyCode === 40  && Trivia.correctAnswers[1]) {
+//    Trivia.answer[i][1] 
+//  }
+//}) 
+//
+//addEventListener('keydown', function(event){
+//  if (event.keyCode === 41) {
+//    Trivia.answer[i][2] 
+//  }
+//}) 
